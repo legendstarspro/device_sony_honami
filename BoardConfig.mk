9ae66106a972ff5cdeb2a44fe24d78212f9cddbb
legendstarspro <legendstarspro@github.com>
2020-03-18 18:48:20 +0000

    Omni-9.0 manifest bring up

---
+++ BoardConfig.mk
@@ -0,0 +1,79 @@
+#
+# Copyright 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := krait
+
+# Binder
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+TARGET_KERNEL_CONFIG    := lineageos_rhine_honami_row_defconfig
+TARGET_KERNEL_SOURCE    := kernel/sony/msm8974
+BOARD_KERNEL_BASE       := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_PAGESIZE   := 2048
+
+BOARD_KERNEL_CMDLINE    := androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE    += user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE    += dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
+BOARD_KERNEL_CMDLINE    += androidboot.selinux=permissive
+
+# lzma compression
+LZMA_RAMDISK_TARGETS := recovery
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2436890624
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12891175936
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
+BOARD_VENDOR_PLATFORM := rhine
+PRODUCT_PLATFORM := rhine
+
+# Recovery
+RECOVERY_SDCARD_ON_DATA := true
+
+# System-as-root
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+
+# TWRP specific build flags
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/wled-backlight/brightness"
+TW_EXCLUDE_TWRPAPP := true
+TW_INCLUDE_CRYPTO := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_MAX_BRIGHTNESS := 255
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
